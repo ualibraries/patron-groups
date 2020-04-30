b3e26b59bfea5442bf76d39290f6632ecb4ad3a7
Jeff Turman <turmanator@email.arizona.edu>
2020-04-29 17:44:35 -0700

    add new run_petl scripts. Refs #12750.

---
+++ run_petl_dev.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# dev version of run script omits sync (important for testing) and slack (no one wants to know)
+
+export $(grep -v '^#' .env | xargs) # get env args mentioned below
+
+for g in faculty-base staff-base students-base ugrads-base grads-base dcc-base retirees-base hsl-base law-base
+do
+    ./src/main/python/petl --config ./src/main/python/config/petl.ini \
+                  --group ${g} \
+                  --ldap_passwd ${PGRPS_LDAP_PASSWD} \
+                  --grouper_passwd ${PGRPS_GROUPER_PASSWD} \
+done
---
+++ run_petl_prod.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export $(grep -v '^#' .env | xargs)
+
+for g in faculty-base staff-base students-base ugrads-base grads-base dcc-base retirees-base hsl-base law-base
+do
+    ./src/main/python/petl --config ./src/main/python/config/petl.ini \
+                  --group ${g} \
+                  --ldap_passwd ${PGRPS_LDAP_PASSWD} \
+                  --grouper_passwd ${PGRPS_GROUPER_PASSWD} \
+                  --slack ${PGRPS_SLACK_WEBHOOK} \
+                  --sync
+done
