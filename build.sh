64307328765760d658052450ac47598765b2ed66
Jeff Turman <turmanator@email.arizona.edu>
2020-05-06 15:24:56 -0700

    update build.sh for testing.

--- build.sh
+++ build.sh
@@ -11,46 +11,52 @@ echo -n "Enter \"dev\" or \"prod\" for package build: "
 read VAR
 
 function git_install_tag {
-    latest_tag = $(git describe --tags `git rev-list --tags --max-count=1`)
+    latest_tag=$( git describe --tags `git rev-list --tags --max-count=1` )
     git fetch --tags
     git checkout $latest_tag
 }
 
 if [[ $VAR == "dev"* ]]; then
-    # update source from github
-    git pull
-
-    pyenv_version = $( head -n 1 ./.python-version ) # get the prod version for local dev
+    pyenv_version=$( head -n 1 ./.python-version ) # get the prod version for local dev
     pyenv install $pyenv_version
 
-    # get the virtualenv and set python version
-    pyenv virtualenvwrapper_lazy
+    # create our virtualenv
+    pyenv virtualenv pgrps-venv
+    python_version=$( python -V )
+    echo -e "Current Python version is $python_version ...\n"
 
-    # make virtual environment available
-    mkvirtualenv venv
-    echo -e "Current Python version is ${ python -v } ...\n"
+    # upgrade pip
+    pip install wheel # weird, pyenv's pip doesn't include this?
+    pip install --upgrade pip
 
     # install package in dev mode
-    echo -e "Installing the Patron Groups package in editable mode... \n"
+    echo -e "Installing the Patron Groups package in editable (dev) mode... \n"
     cd src/main/python
+    pip install --trusted-host pypi.python.org -r requirements.txt
     pip install -e . # install editable package with dependencies
 
+    echo -e "The Patron Groups package has now been installed locally. \n
+            Run './run_petl_dev.sh' to test functionality.\n\n"
+
 elif [[ $VAR == "prod"* ]]; then
-    # TODO: WIP
+    # TODO: WIP!!!
     # actual deployment of package to global pip
 
     echo "Deploying the Patron Groups package... "
 
-    # git pull last tag and match with package version
+    # # git pull last tag
     git_install_tag
 
     # install package in production
     cd src/main/python
-    python setup.py sdist # creates distribution package, version from src/petal/__init__.py::__version__
+    sudo pip3 install --trusted-host pypi.python.org -r requirements.txt
+    python3 setup.py sdist # creates distribution package
 
+    # get version from src/petal/__init__.py::__version__
+    # pgrps_version=$( grep -oP "^__version__ = ['\"]([^'\"]*)['\"]" ./src/petal/__init__.py )
     # pip install absolute path to PG package
 
 else
-    echo "Usage: enter \"dev\" to build local virtualenv or \"prod\" to deploy tagged version... \n"
+    echo "Usage: enter \"dev\" to build local virtualenv or \"prod\" to build tagged version... \n"
 
 fi
