71208650570b64317c4f1b54f830e3d1703e12ec
Jeff Turman <turmanator@email.arizona.edu>
2020-05-08 08:47:43 -0700

    Debugging run scripts. Refs #12750.

--- run_petl_dev.sh
+++ run_petl_dev.sh
@@ -2,12 +2,13 @@
 
 # dev version of run script omits sync (important for testing) and slack (no one wants to know)
 
-export $(grep -v '^#' .env | xargs) # get env args mentioned below
+export $(grep -v '^#' ./.env | xargs) # get env args mentioned below
 
 for g in faculty-base staff-base students-base ugrads-base grads-base dcc-base retirees-base hsl-base law-base
 do
-    ./src/main/python/petl --config ./src/main/python/config/petl.ini \
+    ./src/main/python/scripts/petl --config ./src/main/python/config/petl.ini \
                   --group ${g} \
                   --ldap_passwd ${PGRPS_LDAP_PASSWD} \
-                  --grouper_passwd ${PGRPS_GROUPER_PASSWD} \
+                  --grouper_passwd ${PGRPS_GROUPER_PASSWD}
 done
+
--- run_petl_prod.sh
+++ run_petl_prod.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-export $(grep -v '^#' .env | xargs)
+export $(grep -v '^#' ./.env | xargs)
 
 for g in faculty-base staff-base students-base ugrads-base grads-base dcc-base retirees-base hsl-base law-base
 do
-    ./src/main/python/petl --config ./src/main/python/config/petl.ini \
+    ./src/main/python/scripts/petl --config ./src/main/python/config/petl.ini \
                   --group ${g} \
                   --ldap_passwd ${PGRPS_LDAP_PASSWD} \
                   --grouper_passwd ${PGRPS_GROUPER_PASSWD} \
