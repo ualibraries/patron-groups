7df2cf24ad72fa1b2b06da467a83646eb31a5849
Jeff Turman <turmanator@email.arizona.edu>
2020-05-14 10:57:36 -0700

    update run_petl_dev with python interpreter. Refs #12750

--- run_petl_dev.sh
+++ run_petl_dev.sh
@@ -6,9 +6,8 @@ export $(grep -v '^#' ./.env | xargs) # get env args mentioned below
 
 for g in faculty-base staff-base students-base ugrads-base grads-base dcc-base retirees-base hsl-base law-base
 do
-    ./src/main/python/scripts/petl --config ./src/main/python/config/petl.ini \
+    python ./src/main/python/scripts/petl --config ./src/main/python/config/petl.ini \
                   --group ${g} \
                   --ldap_passwd ${PGRPS_LDAP_PASSWD} \
                   --grouper_passwd ${PGRPS_GROUPER_PASSWD}
 done
-
