ca539f5e53178cb40b540afbfe461b398a871c2f
Jeff Turman <turmanator@email.arizona.edu>
2025-08-06 15:19:06 -0700

    More restructuring. Refs #16449
    Signed-off-by: Jeff Turman <turmanator@email.arizona.edu>

---
+++ src/patron_groups/petal/__init__.py
@@ -0,0 +1,11 @@
+import logging
+
+from .delta import Delta
+from .grouper_query import GrouperQuery
+from .ldap_query import LDAPQuery
+
+#
+# Best practice library logging setup.
+
+logger = logging.getLogger( __name__ )
+logger.addHandler( logging.NullHandler() )
